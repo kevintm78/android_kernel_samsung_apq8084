708168546cbdd5448bd1364d1f1261687068f70c
kevintm78 <kevinmccord.713@gmail.com>
2019-12-18 08:15:29 +0400

    Powersuspend: Fix build

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -33,8 +33,10 @@
 #define POWER_SUSPEND_INACTIVE	0
 #define POWER_SUSPEND_ACTIVE	1
 
+#define POWER_SUSPEND_AUTOSLEEP	0	// Use kernel autosleep as hook
 #define POWER_SUSPEND_USERSPACE	1	// Use fauxclock as trigger
 #define POWER_SUSPEND_PANEL	2	// Use display panel state as hook
+#define POWER_SUSPEND_HYBRID	3	// Use display panel state and autosleep
 
 struct power_suspend {
 	struct list_head link;
